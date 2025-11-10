2321367a1a2250c56b4f5dfd097742e838f2ba2f
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2025-11-11 04:11:49 +0600

    add users post request

--- index.js
+++ index.js
@@ -1,11 +1,75 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000
 
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = "mongodb+srv://habituo-db:x4mjgKZyHtClYmb7@cluster0.wtnhlvb.mongodb.net/?appName=Cluster0";
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    
+    const db = client.db('habituo-db');
+    const habitCollection = db.collection('public-habits')
+    const usersCollection = db.collection('users');
+
+
+
+    app.get('/publicHabits', async(req, res)=>{
+      
+      const result = await habitCollection.find().toArray();
+      
+      res.send(result)
+    })
+
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      if (!user?.email) {
+        return res.status(400).send({ message: "Email is required" });
+      }
+
+      
+      const existing = await usersCollection.findOne({ email: user.email });
+      if (existing) {
+        return res.send({ message: "User already exists", inserted: false });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+   
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
